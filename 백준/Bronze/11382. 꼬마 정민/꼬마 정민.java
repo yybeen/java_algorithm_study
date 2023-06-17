1104e42987e43e7df366a70d674ed5581256491a
yybeen <59864345+yybeen@users.noreply.github.com>
2023-06-18 00:46:31 +0900

    [Bronze V] Title: 꼬마 정민, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        System.out.println(a+b+c);
+    }
+}

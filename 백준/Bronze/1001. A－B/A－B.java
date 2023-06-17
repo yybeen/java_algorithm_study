d4fc15a89a9cd99f56b61e47ccaf4ef7bde01966
yybeen <59864345+yybeen@users.noreply.github.com>
2023-06-18 00:15:51 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        int minus = A-B;
+        System.out.print(minus);
+    }
+}

d5efe9fd93ff3f6e5118065ade662e63021f2db4
yybeen <59864345+yybeen@users.noreply.github.com>
2023-06-19 01:34:14 +0900

    [Bronze V] Title: 개수 세기, Time: 132 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    
+	    int N = Integer.parseInt(br.readLine());
+	    
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] array = new int[N];
+        // List<Integer> result = new ArrayList<>();
+        int result = 0;
+        
+        for(int i = 0; i < N; i++){
+            int value = Integer.parseInt(st.nextToken());
+            
+            array[i] = value;
+        }
+        
+	    int v = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == v){
+                result++;
+            }   
+        }
+        
+        System.out.println(result);
+	}
+}

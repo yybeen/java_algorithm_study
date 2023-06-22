2b000d463d0553387bae6c779ef6fc398fd9c209
yybeen <59864345+yybeen@users.noreply.github.com>
2023-06-22 12:38:34 +0900

    [Bronze V] Title: 검증수, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(s);
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        System.out.println((A*A+B*B+C*C+D*D+E*E)%10);
+    }
+}

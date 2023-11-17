7549bc2514b74c8f3ab432bbf62e60642c55111a
yybeen <59864345+yybeen@users.noreply.github.com>
2023-11-17 16:29:17 +0900

    [Gold V] Title: 빗물, Time: 140 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int maxH = 0;
+		int maxW = 0;
+		int[] height = new int[W];
+		int answer = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < W; i++) {
+		    height[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 1; i < W-1; i++) {
+		    int left = 0;
+		    int right = 0;
+		    
+		    for(int j = 0; j < i; j++) {
+		        left = Math.max(left, height[j]);
+		    }
+		    
+		    for(int j = W-1; j > i; j--) {
+		        right = Math.max(right, height[j]);
+		    }
+		    
+		    if(left!=0 && right!=0 && left>height[i] && right>height[i]) {
+		        if(left >= right) {
+		            answer += right - height[i];
+		        }
+		        else {
+		            answer += left - height[i];
+		        }
+		    }
+		}
+		
+		System.out.println(answer);
+	}
+}
